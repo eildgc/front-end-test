5f3faccadddaa967b3f2688947b8c12cb9b2ab3c
eild <47793204+eildgc@users.noreply.github.com>
2023-07-06 16:08:02 -0600

    Working in mayus issue

--- src/app/components/footerWrapper.tsx
+++ src/app/components/footerWrapper.tsx
@@ -2,7 +2,7 @@
 
 import { isMobile } from "react-device-detect";
 import MobileFooter from "./mobile/footer/footer";
-import Footer from "./desktop2/footer/footer";
+import Footer from "./desktop/footer/footer";
 
 export default function FooterWrapper() {
   return (
