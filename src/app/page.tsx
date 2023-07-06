562f80bcf728f1ecbe4de7bcea5f044966a0ae34
eild <47793204+eildgc@users.noreply.github.com>
2023-07-06 17:39:30 -0600

    Fixed compiled error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-"use client";
+'use client';
 import React, { useEffect, useState } from "react";
 import { isMobile } from "react-device-detect";
 import MobileCarouselCard from "./components/mobile/carouselCard/carouselCard";
